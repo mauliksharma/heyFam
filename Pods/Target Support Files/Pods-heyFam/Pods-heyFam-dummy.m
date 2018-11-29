081c678174cd9a39cf04f98e050c3e68e0a00d31
Maulik Sharma <mauliksharma@Mauliks-MacBook.local>
2018-11-29 16:59:43 +0530

    Installed pods, setup firebase

---
+++ Pods/Target Support Files/Pods-heyFam/Pods-heyFam-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_heyFam : NSObject
+@end
+@implementation PodsDummy_Pods_heyFam
+@end
---
+++ heyFam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  heyFam
+//
+//  Created by Maulik Sharma on 29/11/18.
+//  Copyright © 2018 Geekskool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
